ef1cc2b5bb6df5fa0b1c795aaa58c2948f195bc1
R.T.Rajnesh Durai <rajarajnesh16@gmail.com>
2023-05-26 16:13:15 +0530

    Whole Project

---
+++ APIProjectBigBang/Data/HotelContext.cs
@@ -0,0 +1,14 @@
+﻿using APIProjectBigBang.Authorization;
+using APIProjectBigBang.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIProjectBigBang.Data
+{
+    public class HotelContext:DbContext
+    {
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Register> Register { get; set; }
+        public HotelContext(DbContextOptions<HotelContext> options) : base(options) { }
+    }
+}
